af36bbdb92eebaa797c7be1149f19a9a18dce8d2
Space Cadet <46660453+InterstellarRobo@users.noreply.github.com>
2022-12-03 23:48:56 -0600

    Player input and game loop

--- Main.java
+++ Main.java
@@ -1,15 +1,72 @@
+import java.io.*;
+import java.lang.Thread;
+import org.jline.terminal.*;
+import org.jline.utils.*;
+import org.jline.utils.InfoCmp.Capability;
+
 public class Main {
-    public static void main(String[] args) {
+    private static char input = ' ';
+
+    private static class InputThread implements Runnable{
+        private Terminal t;
+
+        public InputThread(Terminal t) {
+            this.t = t;
+        }
+
+        public void run() {
+            try {
+                Reader r = t.reader();
+                while (!Thread.interrupted()) {
+                    input = (char) r.read();
+                }
+                r.close();
+            }
+            catch (IOException e) {
+                //It's all good baby (-Brian Ehlenbach)
+            }
+        }
+    }
+    public static void main(String[] args) throws InterruptedException, IOException{
+        Terminal terminal = TerminalBuilder.builder().jna(true).system(true).build();
+        terminal.enterRawMode();
+
         World testWorld = new World(15, 15);
-        testWorld.printWorld();
-        System.out.println();
         Snake snake = new Snake (9, 3, 5, testWorld);
-        testWorld.setGameCoord(9, 4, -2);
-        testWorld.printWorld();
-        System.out.println();
-        snake.moveSnakeHead(Direction.DOWN);
-        testWorld.updateWorld();
-        snake.placeSnakeHead();
-        testWorld.printWorld();
+
+        Thread t = new Thread(new InputThread(terminal));
+        t.start();
+
+        while (input != 'q') {
+            switch (input) {
+                case 'w':
+                    snake.moveSnakeHead(Direction.UP);
+                    break;
+                case 'a':
+                    snake.moveSnakeHead(Direction.LEFT);
+                    break;
+                case 's':
+                    snake.moveSnakeHead(Direction.DOWN);
+                    break;
+                case 'd':
+                    snake.moveSnakeHead(Direction.RIGHT);
+                    break;
+                default:
+                snake.moveSnakeHead();
+                    break;
+            }
+
+            testWorld.updateWorld();
+            snake.placeSnakeHead();
+            //Whatever, fix tomorrow
+            terminal.puts(Capability.clear_screen);
+            testWorld.printWorld();
+            
+            Thread.sleep(1000);
+        }
+
+        t.interrupt();
+        t.join();
+        terminal.close();
     }
 }
